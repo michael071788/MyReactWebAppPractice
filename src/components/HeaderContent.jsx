ca068763e6ea02784b13ca7a980a82c638d92e6d
michael071788 <michael071788@gmail.com>
2022-08-18 15:57:53 +0400

    added initial header content

---
+++ src/components/HeaderContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeaderContent = () => {
+   
+    return (
+           <div>
+            HeaderContent 
+           </div>
+    
+    );
+};
+
+export default HeaderContent;
--- src/components/LayoutAdmin.jsx
+++ src/components/LayoutAdmin.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import SideBarContent from "./SideBarContent"
+import HeaderContent from './HeaderContent';
 
 const LayoutAdmin = ({children}) => {
     return (
         <div className="container">
            <SideBarContent />
 
-           <main>{children}</main>
+        <div style={{ display: "flex", flexDirection: "column", width: "100%"}}>
+            <div style={{display: "flex", justifyContent: "flex-end", padding: "15px", boxShadow: "0px 0px 4px #D9DDE7"}}>
+                AAAAAAAAAAAAAAA
+            </div>
+            <div style={{display: "flex",justifyContent: "stretch", height: "100%"}}>
+            <div>{children}</div>
+            </div>
+            
+            </div>
+           
         </div>
     );
 };
