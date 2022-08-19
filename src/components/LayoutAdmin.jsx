b22a99ef9d392e0980f981f33e47a28adfc90e8c
michael071788 <michael071788@gmail.com>
2022-08-19 19:02:42 +0400

    initial user bar

--- src/components/LayoutAdmin.jsx
+++ src/components/LayoutAdmin.jsx
@@ -6,11 +6,26 @@ const LayoutAdmin = ({children}) => {
     return (
         <div style={{display: "flex", flexDirection: "row", width: "100%"}}>
            <SideBarContent />
-
-            <div style={{ display: "flex", width: "100%", backgroundColor: "transparent"}}>
-         
-             <div>{children}</div>
+            <div style={{display: "flex", flexDirection: "column", width: "100%",backgroundColor: "transparent"}}>
+                
+                
+               <div style={{backgroundColor: "transparent",
+                display: "flex",
+                height: "69px",
+                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.05)",
+                justifyContent: "flex-end",
+                alignItems: "center",
+                paddingRight: "25px",
+                }}>User Bar</div> 
+               <div style={{backgroundColor: "transparent"}}>
+               {children}
+               </div> 
+            
+                {/* <div style={{ display: "flex", width: "100%", backgroundColor: "red"}}>
+                    <div></div>
+                </div> */}
             </div>
+          
         </div>
     );
 };
